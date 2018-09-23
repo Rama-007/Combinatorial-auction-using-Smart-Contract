ec679f68caec32daafbfe8eb51ee9063f8b8b6ee
yashwanthyuvi <yashwanthregatte@gmail.com>
2018-09-23 22:15:45 +0530

    added sqrt calculation

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -128,4 +128,17 @@ contract Auction{
         notaries[bidder[msg.sender].notary].input_value=input;
         emit Assignment(msg.sender,bidder[msg.sender].notary);
     }
+    function sqrt(uint x) private returns(uint y)
+    {
+        if (x == 0) return 0;
+        else if (x <= 3) return 1;
+        uint z = (x + 1) / 2;
+        y = x;
+        while (z < y)
+        {
+            y = z;
+            z = (x / z + z) / 2;
+        }
+        return y;
+    }
 }
